638ea11111b1eb01dc3dfb590014391a5e935116
rpf1980 <rafapolo1980@gmail.com>
2019-12-02 17:35:50 +0100

    feat(msi): finished crud SQLite

--- app/src/main/java/com/example/sqliteprueba01/MainActivity.java
+++ app/src/main/java/com/example/sqliteprueba01/MainActivity.java
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void onClick(View v)
             {
-                // Gets the data repository in write mode
+                // Obtiene el repositorio de datos en modo de escritura
                 SQLiteDatabase db = helper.getWritableDatabase();
 
                 // Crea un nuevo mapa de valores para los campos de la base de datos ( inserta un registro de datos )
@@ -56,6 +56,11 @@ public class MainActivity extends AppCompatActivity
                 long newRowId = db.insert(Estructura_BD.TABLE_NAME, null, values);
 
                 Toast.makeText(getApplicationContext(), "Se guardó el registro con clave " + newRowId, Toast.LENGTH_LONG).show();
+
+                //Reseteamos campos
+                edtID.setText("");
+                edtNombre.setText("");
+                edtApellidos.setText("");
             }
         });
 
@@ -65,7 +70,23 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void onClick(View v)
             {
+                // Obtiene el repositorio de datos en modo de escritura
+                SQLiteDatabase db = helper.getWritableDatabase();
 
+                // Nos pide el campo del criterio ( el ID )
+                String selection = Estructura_BD.NOMBRE_COLUMNA1 + " LIKE ?";
+
+                // Capturamos el valor escrito en esa columna1 ( el ID )
+                String[] selectionArgs = { edtID.getText().toString() };
+                // Instrucción SQL
+                int deletedRows = db.delete(Estructura_BD.TABLE_NAME, selection, selectionArgs);
+
+                Toast.makeText(getApplicationContext(), "Se borró el registro con clave: " + edtID.getText().toString(), Toast.LENGTH_LONG).show();
+
+                //Reseteamos campos
+                edtID.setText("");
+                edtNombre.setText("");
+                edtApellidos.setText("");
             }
         });
 
@@ -95,23 +116,33 @@ public class MainActivity extends AppCompatActivity
                 /*String sortOrder =
                         Estructura_BD.COLUMN_NAME_SUBTITLE + " DESC";*/
 
-                // Creación del Objeto de tipo Cursor
-                Cursor cursor = db.query(
-                        Estructura_BD.TABLE_NAME,   // Tabla de la consulta
-                        projection,             // Este parámetro corresponde al array de las columnas que debe devolver
-                        selection,              // Este parámetro corresponde a las columnas que tienen el criterio
-                        selectionArgs,          // Hace referencia de dónde viene el criterio
-                        null,                   // hace referencia a agrupar las consultas por denominación
-                        null,                   // filtrar filas por grupos
-                        null
-                );
+                try
+                {
+                    // Creación del Objeto de tipo Cursor
+                    Cursor cursor = db.query(
+                            Estructura_BD.TABLE_NAME,   // Tabla de la consulta
+                            projection,             // Este parámetro corresponde al array de las columnas que debe devolver
+                            selection,              // Este parámetro corresponde a las columnas que tienen el criterio
+                            selectionArgs,          // Hace referencia de dónde viene el criterio
+                            null,                   // hace referencia a agrupar las consultas por denominación
+                            null,                   // filtrar filas por grupos
+                            null
+                    );
 
-                //Movemos el cursor al primer registro de nuestro resulset
-                cursor.moveToFirst();
+                    //Movemos el cursor al primer registro de nuestro resulset
+                    cursor.moveToFirst();
 
-                //Ahora leemos la información requerida
-                edtNombre.setText(cursor.getString(0));
-                edtApellidos.setText(cursor.getString(1));
+                    //Ahora leemos la información requerida
+                    edtNombre.setText(cursor.getString(0));
+                    edtApellidos.setText(cursor.getString(1));
+                }
+                catch (Exception e)
+                {
+                    Toast.makeText(getApplicationContext(), "No se encontró registro", Toast.LENGTH_LONG).show();
+                }
+
+
+
             }
         });
 
@@ -121,7 +152,31 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void onClick(View v)
             {
+                SQLiteDatabase db = helper.getWritableDatabase();
 
+                // Nuevos valores para los registros
+                String title = "MyNewTitle";
+                ContentValues values = new ContentValues();
+                values.put(Estructura_BD.NOMBRE_COLUMNA2, edtNombre.getText().toString());
+                values.put(Estructura_BD.NOMBRE_COLUMNA3, edtApellidos.getText().toString());
+
+                // Esto sería la columna que vamos a usar como criterio
+                String selection = Estructura_BD.NOMBRE_COLUMNA1 + " LIKE ?";
+                String[] selectionArgs = { edtID.getText().toString() };
+
+                //Método que se encargará de realizar la actualización ( update() )
+                int count = db.update(
+                        Estructura_BD.TABLE_NAME,
+                        values,
+                        selection,
+                        selectionArgs);
+
+                Toast.makeText(getApplicationContext(), "Se actualizó el registro", Toast.LENGTH_LONG).show();
+
+                //Reseteamos campos
+                edtID.setText("");
+                edtNombre.setText("");
+                edtApellidos.setText("");
             }
         });
     }
